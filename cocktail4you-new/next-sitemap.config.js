82a3a385391a96db7ed0347890739fd60ce8e907
littlekanine <elia.kopff3891@gmail.com>
2025-03-18 12:14:17 +0100

    add site.map

---
+++ cocktail4you-new/next-sitemap.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+	siteUrl: 'https://cocktail4you.vercel.app/', // Remplace par ton domaine réel
+	generateRobotsTxt: true, // Cette ligne génère également un fichier robots.txt
+};
